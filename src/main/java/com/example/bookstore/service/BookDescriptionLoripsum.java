80f8faf74052d1d0098131a8e0b1360c933fc5cb
MiroG <miroslaw.golda@gmail.com>
2017-06-03 22:24:24 +0200

    resttemplate

--- src/main/java/com/example/bookstore/service/BookDescriptionLoripsum.java
+++ src/main/java/com/example/bookstore/service/BookDescriptionLoripsum.java
@@ -1,13 +1,25 @@
 package com.example.bookstore.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestOperations;
 
 @Component
 @Qualifier("bookDescriptionLoripsum")
 public class BookDescriptionLoripsum implements BookDescriptionClient {
+
+    private final RestOperations restOperations;
+
+    @Autowired
+    public BookDescriptionLoripsum(RestOperations restOperations) {
+        this.restOperations = restOperations;
+    }
+
     @Override
     public String getDescription(String bookId) {
-        return "loripsum";
+        return restOperations.getForEntity(
+                "http://loripsum.net/api/plaintext", String.class
+        ).getBody();
     }
 }
